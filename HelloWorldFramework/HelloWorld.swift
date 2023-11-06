32b1b50f0ce2da0da43a598527d967ee67ba2664
m-zepeda <mzepeda@xoarmortech.com>
2023-11-06 11:43:42 -0600

    Initial Commit

---
+++ HelloWorldFramework/HelloWorld.swift
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.swift
+//  HelloWorldFramework
+//
+//  Created by Mario  Zepeda on 11/6/23.
+//
+
+import Foundation
+
+public class HelloWorld {
+    public static func sayHello() -> String {
+        return "Hello, World!"
+    }
+}
---
+++ HelloWorldFramework/HelloWorldFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorldFramework.h
+//  HelloWorldFramework
+//
+//  Created by Mario  Zepeda on 11/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorldFramework.
+FOUNDATION_EXPORT double HelloWorldFrameworkVersionNumber;
+
+//! Project version string for HelloWorldFramework.
+FOUNDATION_EXPORT const unsigned char HelloWorldFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldFramework/PublicHeader.h>
+
+
